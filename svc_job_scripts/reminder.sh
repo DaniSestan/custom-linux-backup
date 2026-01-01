dd11561645a784241262f90eaac1f58cbc797bdb
Dani Sestan <daniasestan@gmail.com>
2026-01-01 11:11:14 +0100

    completed reminder systemd timed job config; todo: working on the systemd job executable script

--- svc_job_scripts/reminder.sh
+++ svc_job_scripts/reminder.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
+mkdir -p /var/log/systemd/reminder/
 echo "I am a test script executing at $(date)" | tee -a /var/log/systemd/reminder/reminder.log
+
+# TODO: create a reminder script that can be exec'd from the timed systemd reminder job
+# Setting: low
+# Timing: 5 min - 300000 ms
+# Description: Information about an upcoming backup
+# Details: Notifications set to 'low' should only serve as reminders to the end-user of the conditions of an upcoming backup.
+
+# Setting: normal
+# Timing: 10 min - 600000 ms
+# Description: Information about the status of the current backup
+# Details: Notifications set to 'medium' indicate something of immediate concern about a backup: what's the status of the backup -- has it started, has it stopped? If it has not started, what's the context? If it has stopped without completing successfully, what's the context. Successful start/stop, unsuccessful start/stop if the conditions for a scheduled backup were not met; the backup did not run as intended and must be manually triggered at this point.
+
+# Setting: critical
+# Timing: note that critical notifications need to be manually dismissed, there's no need to set any timer
+# Description: Information about a verified threat or any issue preventing a backup
+# Details: Alerts set to 'high' should be rare: If the data necessary for a backup is not accessible, corrupted or present on the system
